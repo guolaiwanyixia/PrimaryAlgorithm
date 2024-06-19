3c05e94b3fd518f3b46d16391f6aad5066b8e892
lijunjie <lijunjie1737728083@gmail.com>
2024-06-19 21:44:47 +0800

    初级算法题

--- src/初级算法题/数组和字符串算法题/旋转数组.java
+++ src/初级算法题/数组和字符串算法题/旋转数组.java
@@ -1,38 +1,34 @@
 package 初级算法题.数组和字符串算法题;
 
 public class 旋转数组 {
-//
-public static void main(String[] args) {
-    revolve();
-    //revolve2();
-}
-
-public static void revolve(){
-   int [] nums ={1,2,3,4,5,6,7};
-    int tempend = 0;
-    for (int j = 0; j < 3; j++) {
-        tempend = nums[nums.length-1];
-        for (int i = nums.length - 1; i > 0; i--) {
-            nums[i ] = nums[i-1];
-        }
-        nums[0] = tempend;
+    //
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        //前三个数
+        int key = 3;
+        rotate(nums, key);
     }
 
-}
+    public static void rotate(int[] nums, int k) {
+        int[] temp = new int[nums.length];
+        //把数组复制一份
+        for (int i = 0; i < nums.length; i++) {
+            temp[i] = nums[i];
+            System.out.print(temp[i] + " ");
 
-    public static void revolve2() {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int end = nums[nums.length - 1];
-        int tempend = 0;
-        for (int j = 0; j < 3; j++) {
-            tempend = nums[nums.length - 1]; // 将最后一个元素保存到tempend
-            for (int i = nums.length - 1; i > 0; i--) { // 从倒数第二个元素开始向前移动
-                nums[i] = nums[i - 1];
-            }
-            nums[0] = tempend; // 将tempend赋值给第一个元素
+        }
+        System.out.println();
+        //把新数组的值赋给旧数组
+        for (int i = 0; i < nums.length; i++) {
+            //利用取模的性质来巧妙的复制
+            nums[(i + k) % nums.length] = temp[i];
+
         }
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
         }
+
+
+
     }
 }
