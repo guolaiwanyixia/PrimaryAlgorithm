b1e046b27f5d5f83bdf37b45d07005edcf66e332
lijunjie <lijunjie1737728083@gmail.com>
2024-06-19 11:18:59 +0800

    初级算法题

--- src/初级算法题/数组和字符串算法题/两数之和2.java
+++ src/初级算法题/数组和字符串算法题/两数之和2.java
@@ -8,11 +8,12 @@ public class 两数之和2 {
     public static void main(String[] args) {
         int[]nums ={2,7,11,15};
         int target = 9;
-
+       // HashMap  key-value  键值对
         HashMap<Integer, Integer> m = new HashMap<>();
         m.put(nums[0],0);
-        for (int i = 1; i < nums.length; i++) {
 
+        for (int i = 1; i < nums.length; i++) {
+             //检查HashMap里面是否添加过对应的商，
             if(m.containsKey(target-nums[i])){
                 System.out.println(m.get(target-nums[i])+" "+i);
             }
--- src/初级算法题/数组和字符串算法题/旋转数组.java
+++ src/初级算法题/数组和字符串算法题/旋转数组.java
@@ -3,7 +3,36 @@ package 初级算法题.数组和字符串算法题;
 public class 旋转数组 {
 //
 public static void main(String[] args) {
-  //test11111111111111111
-  //test222222222
+    revolve();
+    //revolve2();
 }
+
+public static void revolve(){
+   int [] nums ={1,2,3,4,5,6,7};
+    int tempend = 0;
+    for (int j = 0; j < 3; j++) {
+        tempend = nums[nums.length-1];
+        for (int i = nums.length - 1; i > 0; i--) {
+            nums[i ] = nums[i-1];
+        }
+        nums[0] = tempend;
+    }
+
+}
+
+    public static void revolve2() {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int end = nums[nums.length - 1];
+        int tempend = 0;
+        for (int j = 0; j < 3; j++) {
+            tempend = nums[nums.length - 1]; // 将最后一个元素保存到tempend
+            for (int i = nums.length - 1; i > 0; i--) { // 从倒数第二个元素开始向前移动
+                nums[i] = nums[i - 1];
+            }
+            nums[0] = tempend; // 将tempend赋值给第一个元素
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 }
