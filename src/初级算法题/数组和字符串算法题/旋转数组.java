28d11b763b6d75bb42866fb7987cc01e08f3aacc
lijunjie <lijunjie1737728083@gmail.com>
2024-06-19 22:24:28 +0800

    反转方法，之前版本的名字都没有写对...

--- src/初级算法题/数组和字符串算法题/旋转数组.java
+++ src/初级算法题/数组和字符串算法题/旋转数组.java
@@ -1,34 +1,38 @@
 package 初级算法题.数组和字符串算法题;
 
 public class 旋转数组 {
-    //
+    //反转方法
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
         //前三个数
         int key = 3;
-        rotate(nums, key);
+        int start = 0;
+        int end = nums.length-1;
+        key %= nums.length;
+        //全部反转
+        rotate(nums,start,end);
+        //反转前3个
+        rotate(nums,start,key-1);
+        //反转后4个
+        rotate(nums,key,end);
     }
 
-    public static void rotate(int[] nums, int k) {
-        int[] temp = new int[nums.length];
-        //把数组复制一份
-        for (int i = 0; i < nums.length; i++) {
-            temp[i] = nums[i];
-            System.out.print(temp[i] + " ");
+    public static void rotate(int[] nums,int start,int end) {
+        int temp = 0;
+        //双指针 start，end 反转数组
+        while (start < end){
+            temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--]  = temp;
 
+        }
+
+
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
         }
         System.out.println();
-        //把新数组的值赋给旧数组
-        for (int i = 0; i < nums.length; i++) {
-            //利用取模的性质来巧妙的复制
-            nums[(i + k) % nums.length] = temp[i];
 
-        }
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i] + " ");
-        }
-
-
-
-    }
+}
 }
