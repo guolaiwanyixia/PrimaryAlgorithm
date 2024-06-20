903fdae7965d7efe4fbaecd45280b25fa70c4b4f
lijunjie <lijunjie1737728083@gmail.com>
2024-06-20 14:14:11 +0800

    重复元素-先排序后对比

--- src/初级算法题/数组和字符串算法题/存在重复元素.java
+++ src/初级算法题/数组和字符串算法题/存在重复元素.java
@@ -1,22 +1,29 @@
 package 初级算法题.数组和字符串算法题;
 
-import java.util.HashMap;
+
+
+import java.util.Arrays;
+
 
 public class 存在重复元素 {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,2};
+        int[] nums = {1, 2, 3, 4};
         boolean b = containsDuplicate(nums);
         System.out.println(b);
     }
-    public static boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Integer> HashMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
 
-           if(HashMap.containsKey(nums[i])){
-               return true;
-           }
-            HashMap.put(nums[i], i);
+    public static boolean containsDuplicate(int[] nums) {
+        //先排序
+        Arrays.sort(nums);
+        //排完之后如果有重复的就会并在一起
+        System.out.println(Arrays.toString(nums));
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i]==nums[i+1]){
+                return true;
+            }
+
         }
         return false;
+
     }
 }
