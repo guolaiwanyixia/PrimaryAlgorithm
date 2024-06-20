c66b177dc0acf11f11fb7631ab2bfaea44b0ae8e
lijunjie <lijunjie1737728083@gmail.com>
2024-06-20 09:31:26 +0800

    类似约瑟夫环

--- src/初级算法题/数组和字符串算法题/旋转数组.java
+++ src/初级算法题/数组和字符串算法题/旋转数组.java
@@ -6,33 +6,44 @@ public class 旋转数组 {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
         //前三个数
         int key = 3;
-        int start = 0;
-        int end = nums.length-1;
-        key %= nums.length;
+
         //全部反转
-        rotate(nums,start,end);
-        //反转前3个
-        rotate(nums,start,key-1);
-        //反转后4个
-        rotate(nums,key,end);
+        rotate(nums,key);
+
     }
 
-    public static void rotate(int[] nums,int start,int end) {
-        int temp = 0;
-        //双指针 start，end 反转数组
-        while (start < end){
-            temp = nums[start];
-            nums[start++] = nums[end];
-            nums[end--]  = temp;
+    public static void rotate(int[] nums,int k) {
+        //按照固定的顺序依次向后移动3位
 
+
+        int hold = nums[0];
+        int index = 0;
+        int length = nums.length;
+        boolean[] visited = new boolean[length];
+        for (int i = 0; i < length; i++) {
+            index = (index + k) % length;
+            if (visited[index]) {
+                //如果访问过，再次访问的话，会出现原地打转的现象，
+                //不能再访问当前元素了，我们直接从他的下一个元素开始
+                index = (index + 1) % length;
+                hold = nums[index];
+                i--;
+            } else {
+                //把当前值保存在下一个位置，保存之前要把下一个位置的
+                //值给记录下来
+                visited[index] = true;
+                int temp = nums[index];
+                nums[index] = hold;
+                hold = temp;
+            }
         }
-
-
-
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i]);
         }
-        System.out.println();
 
+        }
 }
-}
+
+
+
+
