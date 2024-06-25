18fd51855887b043f157f7bca322309398b6fcea
lijunjie <lijunjie1737728083@gmail.com>
2024-06-25 21:51:40 +0800

    移动零 - 我的双指针

---
+++ src/初级算法题/数组和字符串算法题/移动零.java
@@ -0,0 +1,35 @@
+package 初级算法题.数组和字符串算法题;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 移动零 {
+    public static void main(String[] args) {
+       int [] nums = {0,1,0,3,12};
+       //       输出: [1,3,12,0,0]
+
+        moveZeroes(nums);
+
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int length = nums.length;
+        int left = 0;
+        int right ;
+
+        for ( right = 0; right <length ; right++) {
+              if(nums[right] != 0){
+                  nums[left]= nums[right];
+                  left++;
+              }
+        }
+
+        for (int temp = left ; temp < length ; temp++) {
+            nums[temp] = 0;
+        }
+        System.out.println(Arrays.toString(nums));
+
+
+    }
+}
