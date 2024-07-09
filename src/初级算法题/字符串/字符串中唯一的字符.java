8bd51bb8dc9ae65279d272922b5f5a7be6a19184
lijunjie <lijunjie1737728083@gmail.com>
2024-07-09 16:16:14 +0800

    字符串中唯一的字符 - HashMap法

--- src/初级算法题/字符串/字符串中唯一的字符.java
+++ src/初级算法题/字符串/字符串中唯一的字符.java
@@ -1,20 +1,24 @@
 package 初级算法题.字符串;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class 字符串中唯一的字符 {
 
-    //字母查找法
+    //HashMap法
     public int firstUniqChar(String s) {
-        int count[] = new int[26];
+        Map<Character, Integer> map = new HashMap();
         char[] chars = s.toCharArray();
-        //先统计每个字符出现的次数
-        for (int i = 0; i < s.length(); i++)
-            count[chars[i] - 'a']++;
+        //先统计每个字符的数量
+        for (char ch : chars) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
         //然后在遍历字符串s中的字符，如果出现次数是1就直接返回
-        for (int i = 0; i < s.length(); i++)
-            if (count[chars[i] - 'a'] == 1)
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(chars[i]) == 1) {
                 return i;
+            }
+        }
         return -1;
     }
 
