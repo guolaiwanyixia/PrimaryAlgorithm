f1f2ed2089076b8cd800c21466b4b00b6d2bdded
lijunjie <lijunjie1737728083@gmail.com>
2024-07-09 16:15:40 +0800

    字符串中唯一的字符 - 字母查找法

--- src/初级算法题/字符串/字符串中唯一的字符.java
+++ src/初级算法题/字符串/字符串中唯一的字符.java
@@ -3,24 +3,19 @@ package 初级算法题.字符串;
 import java.util.HashMap;
 
 public class 字符串中唯一的字符 {
-    public static void main(String[] args) {
-        String s = "leetcode";
-        firstUniqChar(s);
 
-    }
-
-    //官方api方法
-    public static int firstUniqChar(String s) {
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))) {
+    //字母查找法
+    public int firstUniqChar(String s) {
+        int count[] = new int[26];
+        char[] chars = s.toCharArray();
+        //先统计每个字符出现的次数
+        for (int i = 0; i < s.length(); i++)
+            count[chars[i] - 'a']++;
+        //然后在遍历字符串s中的字符，如果出现次数是1就直接返回
+        for (int i = 0; i < s.length(); i++)
+            if (count[chars[i] - 'a'] == 1)
                 return i;
-            }
-
-
-        }
-
         return -1;
-
     }
+
 }
